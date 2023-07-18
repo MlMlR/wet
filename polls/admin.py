8519c9a0b67a8b7cc258fa0315508ca778635104
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-07-18 16:40:24 +0200

    flake8

--- polls/admin.py
+++ polls/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
